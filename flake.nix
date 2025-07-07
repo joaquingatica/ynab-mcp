5d242f25674f139b1518d52ea255e6a445449a06
Joaquin Gatica <me@joaquingatica.com>
2025-07-06 23:54:09 -0300

    build: clean package.json for pack, refactor scripts

--- flake.nix
+++ flake.nix
@@ -2,25 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
+
   outputs = {
     self,
     nixpkgs,
   }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
-    # small tool to iterate over each system
     eachSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system: let
-        pkgs = import nixpkgs { inherit system; };
-      in
-        f pkgs);
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f (import nixpkgs { inherit system; })
+      );
   in {
-    # Used by `nix develop` or `direnv` to open a devShell
-    devShells = eachSystem (pkgs: let
-    in {
+    devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           nodejs
+          jq
         ];
       };
     });
--- pack.sh
+++ pack.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# build ts project
+set -e
 
-rm -rf build
-tsc
+DIST_DIR="dist"
+BIN_DIR="bin"
 
-# copy to build folder
+# copy files to pack to dist folder
 
-rm -rf dist
-mkdir -p dist
-cp -r {assets,build,manifest.json,package.json,package-lock.json} dist
+mkdir -p "$DIST_DIR"
+cp -r {assets,build,manifest.json,package.json,package-lock.json} "$DIST_DIR"
 
-# prepare dist
+# clean package.json install runtime dependencies in dist folder
 
-cd dist || exit 1
-npm ci --omit dev
+cd "$DIST_DIR"
+jq 'del(.main, .scripts, .devDependencies)' package.json > package.tmp.json
+mv package.tmp.json package.json
+npm i --omit dev
+cd ..
 
-# pack
+# pack the dist folder into an extension
 
-cd .. || exit 1
-dxt pack dist bin/ynab.dxt
+dxt pack "$DIST_DIR" "$BIN_DIR/ynab.dxt"
